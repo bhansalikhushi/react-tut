5d2c28a7489f36d6287f658024ece63edf50b6b9
Bhansalikhushi <khushi.jain3055@gmail.com>
2024-04-16 10:36:12 +0530

    created NavBar and HomeBody Components and modified app.js

---
+++ src/components/HomeBody.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomeBody() {
+  return (
+    <div>
+      <h2>home component</h2>
+    </div>
+  );
+}
+
+export default HomeBody;
---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+//to create component rfce
+import React from "react";
+
+function NavBar() {
+  return (
+    <div>
+      <h2>Nav Bar</h2>
+    </div>
+  );
+}
+
+export default NavBar;
